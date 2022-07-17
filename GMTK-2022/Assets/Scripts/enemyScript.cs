daea26fbad372e637c79060b42559a4f426c0958
toxicblacker <ohadk20@gmail.com>
2022-07-17 04:21:35 +0300

    enemy spawner

---
+++ GMTK-2022/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+
+    public float speed;
+    public int hp;
+    public int damage;
+    public int fireRate; //frames between shots
+    public float fireDeg; //degrees of randomness in arc of bullets
+    public float range; //in units
+
+    public float sightRange; //in units
+    public float evasionRange; //in units
+
+    GameObject player;
+
+    private bool firstSpawn = true;
+    private float newSpeed;
+    private int newHp;
+    private int newDamage;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        newSpeed = speed;
+        newHp = hp;
+        newDamage = damage;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 target = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, newSpeed * Time.deltaTime);
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) >= sightRange)
+        {
+            gameObject.transform.position = target;
+        }
+        else if (Vector3.Distance(gameObject.transform.position, player.transform.position) >= evasionRange)
+        {
+            //shoot
+        }
+        else
+        {
+            gameObject.transform.position = gameObject.transform.position - (target - gameObject.transform.position);
+        }
+	}
+    /*
+    public void increaseStats(float mult)
+    {
+        if (firstSpawn)
+        {
+            newSpeed = speed;
+            newHp = hp;
+            newDamage = damage;
+            firstSpawn = false;
+        }
+        //Debug.Log(originalSpeed);
+        //Debug.Log(originalSpeed * mult);
+        newSpeed = speed * mult;
+        newHp = (int)Mathf.Floor(hp * mult);
+        newDamage = (int)Mathf.Floor(damage * mult);
+    }*/
+}
---
+++ GMTK-2022/Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour {
+
+    public int waveTime; //in secs
+    public int firstWaveDelay; //in secs
+    public int spawnRadius; //in units
+    List<int[]> enemyCount = new List<int[]>();
+    public int waveCount;
+    //if endless, wave 1 will be base wave, and enemies will add onto it.
+    public int[] wave1 = new int[4]; //spawns of enemy1, enemy2 ...
+    public GameObject[] enemies = new GameObject[4];
+    public bool isEndless;
+    public float endlessStatMultiplier;
+
+    //public enemyScript[] enemyScripts = new enemyScript[4];
+
+    private float timeLeft;
+    private int i;
+    private System.Random rnd = new System.Random();
+    private Vector3 pos;
+    private int sum;
+	// Use this for initialization
+	void Start () {
+        timeLeft = firstWaveDelay;
+        i = 0;
+        enemyCount.Add(wave1);
+        pos = gameObject.transform.position;
+        for (int w = 0; w < wave1.Length; w++) sum += wave1[w];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0 && i < waveCount && !isEndless)
+        {
+            timeLeft = waveTime;
+
+            for (int j = 0; j < wave1.Length; j++)
+            {
+                for (int k = 0; k < enemyCount[i][j]; k++)
+                {
+                    Instantiate(enemies[j], new Vector3(pos.x + rnd.Next(spawnRadius + 1) - spawnRadius / 2,
+                        pos.y + rnd.Next(spawnRadius + 1) - spawnRadius / 2), Quaternion.identity);
+                }
+            }
+            i++;
+        }
+        else if (timeLeft <= 0 && isEndless)
+        {
+            timeLeft = waveTime;
+            i++;
+
+            if (rnd.Next(1, sum + 1) < wave1[0]) enemyCount[0][0] += 1;
+            else if (rnd.Next(1, sum + 1 - wave1[0]) < wave1[1]) enemyCount[0][1] += 1;
+            else if (rnd.Next(1, sum + 1 - wave1[0] - wave1[1]) < wave1[2]) enemyCount[0][2] += 1;
+            else enemyCount[0][3] += 1;
+
+            for (int j = 0; j < wave1.Length; j++)
+            {
+                for (int k = 0; k < enemyCount[0][j]; k++)
+                {
+                    //enemyScripts[j].increaseStats(Mathf.Pow(endlessStatMultiplier, i));
+                    Instantiate(enemies[j], new Vector3(pos.x + rnd.Next(spawnRadius + 1) - spawnRadius / 2,
+                        pos.y + rnd.Next(spawnRadius + 1) - spawnRadius / 2), Quaternion.identity);
+                }
+            }
+
+            
+        }
+	}
+}
